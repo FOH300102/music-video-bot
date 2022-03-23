24571bb131eb635be3711fab5e5a8099afdea8ee
LOGESH <95044430+LOGI-LAB@users.noreply.github.com>
2022-03-23 21:24:10 +0530

    Update Playlist.py

--- logi/Plugins/Playlist.py
+++ logi/Plugins/Playlist.py
@@ -3,19 +3,19 @@ from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,
                             KeyboardButton, Message, ReplyKeyboardMarkup,
                             ReplyKeyboardRemove)
 
-from Yukki import BOT_ID, BOT_USERNAME, MUSIC_BOT_NAME, SUDOERS, app, db_mem
-from Yukki.Database import (_get_playlists, delete_playlist, get_playlist,
+from logi import BOT_ID, BOT_USERNAME, MUSIC_BOT_NAME, SUDOERS, app, db_mem
+from logi.Database import (_get_playlists, delete_playlist, get_playlist,
                             get_playlist_names, save_playlist)
-from Yukki.Decorators.admins import AdminRightsCheck
-from Yukki.Decorators.assistant import AssistantAdd
-from Yukki.Decorators.checker import checker, checkerCB
-from Yukki.Decorators.permission import PermissionCheck
-from Yukki.Inline import (add_genre_markup, check_genre_markup, check_markup,
+from logi.Decorators.admins import AdminRightsCheck
+from logi.Decorators.assistant import AssistantAdd
+from logi.Decorators.checker import checker, checkerCB
+from logi.Decorators.permission import PermissionCheck
+from logi.Inline import (add_genre_markup, check_genre_markup, check_markup,
                           delete_playlist_markuup, download_markup,
                           others_markup, play_genre_playlist, playlist_markup,
                           third_playlist_markup)
 
-__MODULE__ = "Playlist"
+__MODULE__ = "✨ᴘʟᴀʏʟɪsᴛ✨"
 __HELP__ = """
 
 
@@ -164,7 +164,7 @@ options_Genre = [
     "Rock",
     "Sad",
     "Party",
-    "Lofi",
+    "Logi",
     "Bollywood",
     "Hollywood",
     "Punjabi",
