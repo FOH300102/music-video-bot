531b205d7fbea714179f80b378fb6a127953415a
LOGESH <95044430+LOGI-LAB@users.noreply.github.com>
2022-03-23 22:31:30 +0530

    Update __main__.py

--- logi/__main__.py
+++ logi/__main__.py
@@ -17,7 +17,7 @@ from logi import (ASS_CLI_1, ASS_CLI_2, ASS_CLI_3, ASS_CLI_4, ASS_CLI_5,
                    ASSNAME3, ASSNAME4, ASSNAME5, BOT_ID, BOT_NAME, LOG_CLIENT,
                    OWNER_ID, app)
 from logi.Core.Clients.cli import LOG_CLIENT
-from logi.Core.PyTgCalls.Yukki import (pytgcalls1, pytgcalls2, pytgcalls3,
+from logi.Core.PyTgCalls.logi import (pytgcalls1, pytgcalls2, pytgcalls3,
                                         pytgcalls4, pytgcalls5)
 from logi.Database import (get_active_chats, get_active_video_chats,
                             get_sudoers, is_on_off, remove_active_chat,
@@ -60,7 +60,7 @@ async def initiate_bot():
         )
         for all_module in ALL_MODULES:
             imported_module = importlib.import_module(
-                "Yukki.Plugins." + all_module
+                "logi.Plugins." + all_module
             )
             if (
                 hasattr(imported_module, "__MODULE__")
